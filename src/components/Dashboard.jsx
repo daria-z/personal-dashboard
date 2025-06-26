ee70631d1471d03e6d956a0857b847484774ebac
Daria Zakirova <daria.zakirova@gmail.com>
2025-06-26 22:42:40 +0300

    feat: add Dashboard component

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,18 @@
+import styles from "./Dashboard.module.css";
+import WeatherCard from "./WeatherCard";
+
+export default function Dashboard() {
+  return (
+    <div className={styles.appContainer}>
+      <header>
+        <h1></h1>
+      </header>
+      <main className={styles.cardsContainer}>
+        <WeatherCard />
+      </main>
+      <footer>
+
+      </footer>
+    </div>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import WeatherCard from './WeatherCard.jsx'
+import Dashboard from './components/Dashboard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <WeatherCard />
-  </StrictMode>,
+    <Dashboard />
+  </StrictMode>
 )
