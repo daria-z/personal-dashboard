19ee3d9698de11193c6c44844dfc5570f2dc9890
daria.daria <33233023+daria-z@users.noreply.github.com>
2025-07-26 16:35:33 +0500

    feat: show form data

--- src/components/Form/FormComponent.jsx
+++ src/components/Form/FormComponent.jsx
@@ -6,33 +6,44 @@ const FormComponent = () => {
     email: '',
   });
 
+  const [isSubmitted, submitData] = useState(false);
+
   const handleChange = (e) => {
     const { value, name } = e.target;
     setFormData((prev) => ({ ...prev, [name]: value }));
   };
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData);
+    submitData(true);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <FormField
-        label="Name"
-        name="name"
-        value={formData.name}
-        onChange={handleChange}
-        error={false}
-      />
-      <FormField
-        label="Email"
-        name="email"
-        value={formData.email}
-        onChange={handleChange}
-        error={false}
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <FormField
+          label="Name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          error={false}
+        />
+        <FormField
+          label="Email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          error={false}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {isSubmitted && (
+        <div>
+          <h3>Отправленные данные</h3>
+          <p>Name: {formData.name}</p>
+          <p>Email: {formData.email}</p>
+        </div>
+      )}
+    </>
   );
 };
 
